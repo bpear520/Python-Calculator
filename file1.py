37d4a703bc30c181e7462308383361425ca30742
Brock <bpear520@mtroyal.ca>
2022-04-26 11:00:06 -0600

    Creating basic calculator functions

---
+++ file1.py
@@ -0,0 +1,32 @@
+# dev 1
+def add(a, b):
+    return a + b
+
+# dev 2
+def multiply(a, b):
+    return a * b
+
+# dev 3
+def divide(a, b):
+    return a / b
+
+# dev 4
+def sub(a, b):
+    divide(1, 2)
+    return a - b
+
+
+def exponent(a, b):
+    return a ** b
+
+
+def print_our_results(a, b, calculation_type):
+    print(calculation_type(a, b))
+
+
+def integrate(a, b):
+    return a + b
+
+
+def modulus(a, b):
+    return a % b
